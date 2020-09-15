50fd7392217fbf6066d542bcbd13dd0b175b65cf
Ted Cook <teodoro.cook@gmail.com>
2020-09-14 22:42:48 -0600

    Refactor setup playbook

---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+git_clone_url=https://github.com/nephelaiio/ansible-role-bash.git
+OK=0
+KO=1
+
+# redefine pushd/popd
+# see: https://stackoverflow.com/questions/25288194/dont-display-pushd-popd-stack-across-several-bash-scripts-quiet-pushd-popd
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+# usage helper
+function help {
+    echo "$0 OPTIONS [ANSIBLE ARGUMENTS]"
+    echo
+    echo "OPTIONS:"
+    echo "   [--local]"
+}
+
+# parse options
+# see https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        --local)
+            LOCAL=$OK
+            shift # past argument
+            ;;
+        *)    # unknown option
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
+    esac
+done
+
+# verify requirements
+requirements=(ansible-playbook git)
+for r in ${requirements[@]}; do
+    if ! r_path=$(type -p $r); then
+        echo "$r executable not found in path, aborting"
+        exit $KO
+    fi
+done
+
+# perform local role install
+sudo apt install -y libvirt-dev
+pip install -r requirements.txt
+if [ -f roles/requirements.yml ] ; then
+    ansible-galaxy install -r roles/requirements.yml --force
+fi
+if [ -f collections/requirements.yml ] ; then
+    ansible-galaxy collection install -r collections/requirements.yml --force
+fi
+
+# purge temp files
+exit $OK
